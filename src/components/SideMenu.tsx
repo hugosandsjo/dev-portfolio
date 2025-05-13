d4061e8f6dbb5b85838a6b99da9e8d424d1013ec
hugosandsjo <hugosandsjo@gmail.com>
2025-05-13 14:49:25 +0200

    Implement code changes to enhance functionality and improve performance

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -8,20 +8,30 @@ export default function SideMenu() {
           With a background in arts and crafts i bring my knowledge in classic
           design techniques into the digital landscape. I get excited about
           visual problem solving and depending on the needs of the project I use
-          graphic design, illustration and animation to deliver the desired
-          result.
+          design, code and animation to deliver the desired result.
         </p>
       </div>
       <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-semibold">About</h1>
+        <h1 className="text-2xl font-semibold">Education</h1>
         <hr className="border-t-2 border-black" />
-        <p className="text-md leading-[1.5] font-light">
-          With a background in arts and crafts i bring my knowledge in classic
-          design techniques into the digital landscape. I get excited about
-          visual problem solving and depending on the needs of the project I use
-          graphic design, illustration and animation to deliver the desired
-          result.
-        </p>
+        <div className="flex flex-col gap-1">
+          <h4 className="text-xl font-medium">YRGO</h4>
+          <div className="flex justify-between">
+            <h5 className="text-md font-light">Web developer</h5>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h4 className="text-xl font-medium">Kunsthøgskolen i Oslo</h4>
+          <div className="flex gap-1 items-baseline">
+            <h5 className="text-md font-light">Bachelor in Graphic Design</h5>
+          </div>
+        </div>
+        <div className="flex flex-col gap-1">
+          <h4 className="text-xl font-medium">School Of Motion</h4>
+          <div className="flex gap-1 items-baseline">
+            <h5 className="text-md font-light">Animation bootcamp</h5>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col gap-4">
         <hr className="border-t-2 border-black" />
--- src/components/WorkGrid.tsx
+++ src/components/WorkGrid.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 export default function WorkGrid() {
@@ -7,7 +8,15 @@ export default function WorkGrid() {
       <hr className="border-t-2 border-black" />
       <div className="grid sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-8">
         <div className="flex flex-col gap-2">
-          <div className="bg-black h-[240px] w-full fade-div"></div>
+          <div className="relative h-[280px] w-full">
+            <Image
+              src={"/fullkoll/fullkoll_main.jpg"}
+              alt="Image"
+              fill
+              className="object-cover absolute object-center"
+              priority
+            />
+          </div>
           <div className="flex flex-col gap-1 fade-div">
             <h2 className="text-2xl font-semibold">Full koll!</h2>
             <h3 className="text-md opacity-60 font-regular italic">
@@ -15,17 +24,27 @@ export default function WorkGrid() {
             </h3>
           </div>
           <p className="text-md leading-[1.5] font-light fade-div">
-            With a background-div arts and crafts i bring my knowledge-div
-            classic design techniques into the digital landscape. I get excited
-            about visual problem solving and depending on the...
+            Together with Gullersgrupp, I developed a set of illustrations and
+            visual identity for the County Administrative Board and the 'Full
+            Koll’ campaign. Intended to inform teenage parents about setting
+            boundaries around alcohol and drugs and the importance of talking
+            about the subject.
           </p>
         </div>
         <div className="flex flex-col gap-2 fade-div">
-          <div className="bg-black h-[240px] w-full"></div>
+          <div className="relative h-[280px] w-full">
+            <Image
+              src={"/dumkeramik/dumkeramik.png"}
+              alt="Image"
+              fill
+              className="object-cover absolute object-center"
+              priority
+            />
+          </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-2xl font-semibold">Full koll!</h2>
+            <h2 className="text-2xl font-semibold">Dum keramik</h2>
             <h3 className="text-md opacity-60 font-regular italic">
-              Illustration
+              Development
             </h3>
           </div>
           <p className="text-md leading-[1.5] font-light">
@@ -35,11 +54,19 @@ export default function WorkGrid() {
           </p>
         </div>
         <div className="flex flex-col gap-2 fade-div">
-          <div className="bg-black h-[240px] w-full"></div>
+          <div className="relative h-[280px] w-full">
+            <Image
+              src={"/reel/Instagram_birdandgirl.gif"}
+              alt="Image"
+              fill
+              className="object-cover absolute object-center"
+              priority
+            />
+          </div>
           <div className="flex flex-col gap-1">
-            <h2 className="text-2xl font-semibold">Full koll!</h2>
+            <h2 className="text-2xl font-semibold">Motion reel</h2>
             <h3 className="text-md opacity-60 font-regular italic">
-              Illustration
+              Animation
             </h3>
           </div>
           <p className="text-md leading-[1.5] font-light">
